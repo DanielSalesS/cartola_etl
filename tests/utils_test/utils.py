b59bc34b319ae3c9830ad4955dc5b97bf2120b7c
DanielSalesS <dan_fk2dan@hotmail.com>
2023-09-11 14:39:21 -0300

    Update path of temporary test files

--- tests/utils_test/utils.py
+++ tests/utils_test/utils.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from cartola_etl.config.etl_config import BASE_YEAR
 
 
 def get_root_test_path():
@@ -7,6 +8,6 @@ def get_root_test_path():
 
 def get_test_staging_area_path():
     test_path = get_root_test_path()
-    test_path = test_path.joinpath("test_files", "staging_area")
+    test_path = test_path.joinpath("test_files", "staging_area", "raw_data", BASE_YEAR)
     test_path.mkdir(parents=True, exist_ok=True)
     return test_path
