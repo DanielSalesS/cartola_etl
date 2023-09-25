6c83d2332b5f9e0823a99c460eae063bceece83f
DanielSalesS <dan_fk2dan@hotmail.com>
2023-09-25 11:21:08 -0300

    Add install requires

--- setup.py
+++ setup.py
@@ -6,4 +6,11 @@ setup(
     description="ETL para coleta e processamento de dados da API do Cartola",
     author="Daniel Sales",
     packages=find_packages(),
+    install_requires=[
+        "python-dotenv==1.0.0",
+        "requests==2.31.0",
+        "mysql-connector-python==8.1.0",
+        "pandas==2.0.3",
+        "numpy==1.24.4",
+    ],
 )
